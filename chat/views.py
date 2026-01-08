559505a16e0486e76b6b0c3280a7fb0bbd5d1d03
Elite Dev <oyenekantomiwa2.com>
2026-01-09 00:20:10 +0100

    user auth applied

--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
 # Create your views here.
-# @login_required(login_url='login')
+
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
 
 
 def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html')
     return render(request, 'login.html')
 
 
@@ -17,7 +27,18 @@ def logoutUser(request):
     return redirect('login')
 
 
-def registerUser(request):
+def registerUser(request): 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+        if password != password2:
+            return render(request, 'register.html')
+        user = User.objects.create_user(username=username, password=password, email=email)
+        user.save()
+        login(request, user)
+        return redirect('index')
     return render(request, 'register.html')
 
 def chatRooms(request):
