0d6dd91a25b79960a0f22cdba8e1a95ea43872d1
Elite Dev <oyenekantomiwa2.com>
2026-01-09 20:54:05 +0100

    done 4real

--- Chatapp/settings.py
+++ Chatapp/settings.py
@@ -91,19 +91,20 @@ DATABASES = {
     'default': dj_database_url.config(
         default='postgresql://postgres.uwpbeniwfksyszqfrbiq:iwa1921post..@aws-1-eu-west-1.pooler.supabase.com:6543/postgres',
         conn_max_age=600,
+        ssl_require=True
     )
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+#}
 
 
 # Password validation
